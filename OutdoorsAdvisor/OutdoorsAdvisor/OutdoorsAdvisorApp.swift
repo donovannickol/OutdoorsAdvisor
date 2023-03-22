646f6b861b21234571961a34635c2480af22dcd7
Akash Mullick <akash.mullick@duke.edu>
2023-03-22 10:32:42 -0400

    initial Xcode commit

---
+++ OutdoorsAdvisor/OutdoorsAdvisor/OutdoorsAdvisorApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutdoorsAdvisorApp.swift
+//  OutdoorsAdvisor
+//
+//  Created by Akash Mullick on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct OutdoorsAdvisorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
